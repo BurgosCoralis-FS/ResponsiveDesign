a65ca42a555a22215c049754cc1501099fb89bcd
Cora <csburgosrodriguez@student.fullsail.edu>
2024-06-10 13:25:27 -0400

    upon toggle hero and nav content changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,14 @@ const scrollUp = document.querySelector("#scroll-up");
 const checkbox = document.querySelector("#checkbox");
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("ul");
+const navTitle = document.querySelector("#nav-title");
 const navLink = document.querySelectorAll("#nav-link");
+const headerTextHear = document.querySelector(".intro-text .hear");
+const headerTextConnecting = document.querySelector(".intro-text .connecting");
+const headerTextSub = document.querySelector(".intro-text .sub");
+const blckBtnText = document.querySelector(".intro-text .black");
+const blueBtnText = document.querySelector(".intro-text .blue");
+const iframe = document.querySelector(".i-frame iframe");
 
 scrollUp.addEventListener("click", () => {
     window.scrollTo({
@@ -13,8 +20,41 @@ scrollUp.addEventListener("click", () => {
 });
 
 checkbox.addEventListener("change", () => {
-  // Toggle website theme
+    // Toggle website theme
     document.body.classList.toggle("dark");
+
+    // Update header text based on theme
+    if (document.body.classList.contains("dark")) {
+        navTitle.textContent = "The Abysmal EP";
+        navLink.forEach((link) => {
+            if (link.textContent === "Perfect") {
+                link.textContent = "Abysmal";
+            }
+        });
+        headerTextHear.textContent = "Let me ask you this question";
+        headerTextConnecting.textContent = "Before you leave";
+        headerTextSub.textContent = "I plead";
+        blckBtnText.textContent = "Save me";
+        blueBtnText.textContent = "from these emotions";
+
+        // Change iframe source
+        iframe.src = "https://www.youtube.com/embed/2KmKl_gRlDs?si=-_qnijLBgJ0vw_U5";
+    } else {
+        navTitle.textContent = "perfect blues";
+        navLink.forEach((link) => {
+            if (link.textContent === "Abysmal") {
+                link.textContent = "Perfect";
+            }
+        });
+        headerTextHear.textContent = "Playing the perfect blues";
+        headerTextConnecting.textContent = "Happy moments, take a breath";
+        headerTextSub.textContent = "Singing a perfect tune";
+        blckBtnText.textContent = "Feeling creatures";
+        blueBtnText.textContent = "in the depth";
+
+        // Change iframe source back to original
+        iframe.src = "https://www.youtube.com/embed/M-9PLDrC_vQ?si=FhA4hS-BRNRNuoy1";
+    }
 });
 
 hamburger.addEventListener("click", openMenu);
@@ -25,6 +65,7 @@ function openMenu() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
